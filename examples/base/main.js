1ea411f7d24512e2660735e79727e7e4cb607b29
Tim Schaub <tim.schaub@gmail.com>
2017-06-21 17:32:46 -0700

    Use prettier

--- examples/base/main.js
+++ examples/base/main.js
@@ -2,28 +2,32 @@ var fs = require('fs');
 var path = require('path');
 
 function main(paths) {
-  return Promise.all(paths.map(function(name) {
-    return new Promise(function(resolve, reject) {
-      fs.readFile(name, function(err, data) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(String(data));
-        }
+  return Promise.all(
+    paths.map(function(name) {
+      return new Promise(function(resolve, reject) {
+        fs.readFile(name, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(String(data));
+          }
+        });
       });
-    });
-  }));
+    })
+  );
 }
 
 if (require.main === module) {
   var paths = ['main.js', '.eslintrc'].map(function(name) {
     return path.join(__dirname, name);
   });
-  main(paths).then(function(contents) {
-    contents.forEach(function(content, index) {
-      process.stdout.write(paths[index] + '\n' + content + '\n');
+  main(paths)
+    .then(function(contents) {
+      contents.forEach(function(content, index) {
+        process.stdout.write(paths[index] + '\n' + content + '\n');
+      });
+    })
+    .catch(function(err) {
+      process.stderr.write('Failed: ' + err.message + '\n');
     });
-  }).catch(function(err) {
-    process.stderr.write('Failed: ' + err.message + '\n');
-  });
 }
